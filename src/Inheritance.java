f399a6895a8c6f22da3024935ad29b001456c9b5
vibhagurjar <vibhagurjar4983@gmail.com>
2024-04-17 12:12:53 +0530

    encapsulation added

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,16 +1,18 @@
 class Animal1{
     void walk(){System.out.println("walking...");}
 }
-class Dog1 extends Animal{
+class Dog1 extends Animal1{
     void bark(){System.out.println("barking...");}
 }
-class Cat extends Animal{
+class Cat extends Animal1{
     void meow(){System.out.println("meowing...");}
 }
-class Inheritance{
+ class Inheritance{
     public static void main(String args[]){
         Cat c=new Cat();
         c.meow();
+        c.walk();
+
     }
 }
 
