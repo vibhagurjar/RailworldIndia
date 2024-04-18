002724573bd8020c0ba2f546c3264aefaba67a61
vibhagurjar <vibhagurjar4983@gmail.com>
2024-04-18 15:49:02 +0530

    array practice question 1

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Stack;
+
 public class ReverseArray {
     //Reverse an array without using an extra space
     public static void RevArray(int arr[],int start,int end){
@@ -12,6 +14,21 @@ public class ReverseArray {
             end--;
 
         }
+        System.out.println("Array after reverse without using extra space.");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    static void RevArrayUsingStack(int arr[]){
+        Stack<Integer> s = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            s.push(arr[i]);
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i] = s.pop();
+        }
+
+        System.out.println("Array after reverse using stack");
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
@@ -19,6 +36,13 @@ public class ReverseArray {
 
     public static void main(String[] args) {
         int arr[]= {1,2,3,4,5};
+        System.out.println("Array before reverse.");
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
         RevArray(arr,0,4);
+//        RevArrayUsingStack(arr);
     }
 }
